851c8065664382f15f072a354090715b926e5905
Ashfaqul Awal <ashfaq.awal@gmail.com>
2024-04-16 20:32:51 +0200

    Setting properties for the car.
    - Setting properties for the car.
    - Creating constructor to initialize the car.

--- CarRace.cs
+++ CarRace.cs
@@ -1,11 +1,25 @@
 ﻿using System;
+using System.Threading;
+
 namespace WorkingWithThreads
 {
-	public class CarRace
+    // CarRace class representing a single car in the race
+    public class CarRace
 	{
-		public CarRace()
-		{
-		}
-	}
+        // Properties of the car
+        public string Name { get; }
+        public int Distance { get; private set; }
+        public int Speed { get; private set; }
+        public bool HasProblem { get; private set; }
+
+        // Constructor to initialize the car
+        public CarRace(string name)
+        {
+            Name = name;
+            Distance = 0;
+            Speed = 120;
+            HasProblem = false;
+        }
+    }
 }
 
